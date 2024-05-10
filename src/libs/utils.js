620c874d68f604c8e1563ea28a7dbee1de11ab0f
wujiang <wujiang@weli.cn>
2024-05-10 17:00:12 +0800

    Enhancement:代理ID更改

--- src/libs/utils.js
+++ src/libs/utils.js
@@ -2,7 +2,7 @@
  * @Author: wujiang@weli.cn
  * @Date: 2024-02-28 16:49:35
  * @LastEditors: wujiang
- * @LastEditTime: 2024-05-10 16:39:04
+ * @LastEditTime: 2024-05-10 16:57:47
  * @Description: 工具函数
  */
 import moment from 'moment';
@@ -1407,8 +1407,8 @@ const resetResultUrl = (order_id, status, is_home_page = false) => {
 // import { fb } from 'facebook-pixel';
 const channel_obj = {
   own: '709060547838898',
-  nova01: '1521267251796283',
-  enjoy01: '953933459469054',
+  nova01: '718889796870693',
+  enjoy01: '1122026312239443',
 };
 // fb.config({
 //   autoPageView: true,
