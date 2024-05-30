663d819be641aaad37da4399e273f4bb46d425c6
wujiang <wujiang@weli.cn>
2024-05-30 10:19:58 +0800

    BugFix

--- src/libs/utils.js
+++ src/libs/utils.js
@@ -2,7 +2,7 @@
  * @Author: wujiang@weli.cn
  * @Date: 2024-02-28 16:49:35
  * @LastEditors: wujiang
- * @LastEditTime: 2024-05-29 19:04:15
+ * @LastEditTime: 2024-05-30 10:19:26
  * @Description: 工具函数
  */
 import moment from 'moment';
@@ -29,7 +29,7 @@ const getFBChannel = () => {
   } else if (url.indexOf('panda03') > -1) {
     return 'panda03';
   } else {
-    return 'enjoy01';
+    return '';
   }
 };
 /**
