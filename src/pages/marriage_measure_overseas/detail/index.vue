3f1fa591307b2c585d0f07ebf9c74e210754013c
liushuai <liushuai@weli.cn>
2024-06-05 18:03:16 +0800

    BugFix:修复已知bug

--- src/pages/marriage_measure_overseas/detail/index.vue
+++ src/pages/marriage_measure_overseas/detail/index.vue
@@ -268,7 +268,7 @@ export default {
     window.scrollTo(0, 0);
     this.$nextTick(() => {
       //排除渠道3
-      if(!is_show_combination) return
+      if(!this.is_show_combination) return
       // 滚动到指定元素
       const element = document.getElementById('title-pay');
       if (element) {
