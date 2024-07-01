0b6a1b2c1d0efc796cf0591b1a677e62bd1fe5d4
liushuai <liushuai@weli.cn>
2024-07-01 14:05:30 +0800

    feat:唤起默认浏览器验证

--- src/components/PayDetail.vue
+++ src/components/PayDetail.vue
@@ -573,8 +573,9 @@ export default {
           localStorage.removeItem('mlxz_fixed_api_order_time');
         }
         await utils.asleep(1000);
-        location.href = res.data.pay_url;
-        // window.open(res.data.pay_url, '_blank');
+        // location.href = res.data.pay_url;
+        console.log('window', window)
+        window.open(res.data.pay_url, '_blank');
       }
     },
   },
