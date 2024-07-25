bf0e792616fb5ef345606260c2088a6a89f6220c
liushuai <liushuai@weli.cn>
2024-07-25 13:37:41 +0800

    feat:删除待支付埋点

--- src/components/TarotPayItem.vue
+++ src/components/TarotPayItem.vue
@@ -130,11 +130,11 @@ export default {
         if (!utils.getQueryStr('mlxz_outer_visitor_id')) {
           location.href += `&mlxz_outer_open_uid=${localStorage.getItem('mlxz_outer_open_uid')}&mlxz_outer_access_token=${localStorage.getItem('mlxz_outer_access_token')}&mlxz_outer_visitor_id=${localStorage.getItem('mlxz_outer_visitor_id')}&question=${encodeURIComponent(localStorage.getItem('question'))}&selected_card_list=${encodeURIComponent(localStorage.getItem('selected_card_list'))} &_fbc=${localStorage.getItem('_fbc')}&_fbp=${localStorage.getItem('_fbp')}&timestamp=${new Date().getTime()}` 
         }
-        utils.firebaseLogEvent(modal_info[this.product_key]['module'], modal_info[this.product_key]['content_id'], modal_info[this.product_key]['event_name'], modal_info[this.product_key]['type'], {
-          args_name: modal_info[this.product_key]['event_name'],
-          channel: utils.getFBChannel(),
-          os: utils.isAndroid() ? 'android' : 'ios'
-        });
+        // utils.firebaseLogEvent(modal_info[this.product_key]['module'], modal_info[this.product_key]['content_id'], modal_info[this.product_key]['event_name'], modal_info[this.product_key]['type'], {
+        //   args_name: modal_info[this.product_key]['event_name'],
+        //   channel: utils.getFBChannel(),
+        //   os: utils.isAndroid() ? 'android' : 'ios'
+        // });
         return
       }
       Indicator.open('订单创建中');
