157e1249b2e5100653931891e455a14103970dae
liushuai <liushuai@weli.cn>
2024-07-25 19:08:20 +0800

    BugFix:ui走查问题

--- src/pages/tarot/order/index.vue
+++ src/pages/tarot/order/index.vue
@@ -560,7 +560,7 @@ export default {
             max-height: 100%;
             border-radius: 0.12rem;
             background: #2C263B;
-            padding: 0.1rem 0.08rem;
+            padding: 0.1rem 0.2rem;
 
             .text{
               display: -webkit-box;
