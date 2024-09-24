61627bcb4f0ce849d8c91b6b65fe3cb9c154516c
liushuai <liushuai@weli.cn>
2024-09-24 15:35:55 +0800

    feat:放开fb事件上报

--- src/pages/emotion_remarriage/result/index.vue
+++ src/pages/emotion_remarriage/result/index.vue
@@ -390,7 +390,8 @@ export default {
           mlxz_order_status: report_status,
         });
         console.log('Purchase事件上报', this.order_id)
-        if (utils.isProd()) {
+        // if (utils.isProd()) {
+          if (1===1) {
           await utils.checkFB();
           try {
             utils.gcyLog(`order_id:${this.order_id}`, {
@@ -399,10 +400,10 @@ export default {
               mlxz_currency: currency_type,
               mlxz_order_status: report_status,
             });
-            // fbq('track', 'Purchase', {
-            //   value: report_price.toFixed(2),
-            //   currency: currency_type,
-            // }, { eventID: this.order_id });
+            fbq('track', 'Purchase', {
+              value: report_price.toFixed(2),
+              currency: currency_type,
+            }, { eventID: this.order_id });
             utils.gcyLog(`order_id:${this.order_id}`, {
               mlxz_action_desc: '完成FB埋点上报，Purchase',
               mlxz_value: report_price.toFixed(2),
