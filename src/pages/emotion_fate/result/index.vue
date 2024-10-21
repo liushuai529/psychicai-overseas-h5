11d99d8d954227e20ec9fa454e18be39f71c6c50
liushuai <liushuai@weli.cn>
2024-10-21 13:47:34 +0800

    feat:goole事件上报

--- src/pages/emotion_fate/result/index.vue
+++ src/pages/emotion_fate/result/index.vue
@@ -415,16 +415,16 @@ export default {
               value: report_price.toFixed(2),
               currency: currency_type,
             }, { eventID: this.order_id });
-            gtag && gtag("event", "purchase", {
-              transaction_id: this.order_id,
-              value: report_price.toFixed(2),
-              currency: currency_type, 
-              items: [
-                {
-                  item_id: product_id, 
-                }
-              ]
-            })
+            // gtag && gtag("event", "purchase", {
+            //   transaction_id: this.order_id,
+            //   value: report_price.toFixed(2),
+            //   currency: currency_type, 
+            //   items: [
+            //     {
+            //       item_id: product_id, 
+            //     }
+            //   ]
+            // })
             utils.gcyLog(`order_id:${this.order_id}`, {
               mlxz_action_desc: '完成FB埋点上报，Purchase',
               mlxz_value: report_price.toFixed(2),
